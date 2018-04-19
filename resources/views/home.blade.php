954687283d24929e60de45c9a3b2ef16d9b3aac8
Bhushan Mer <bhushanm@meditab.com>
2018-04-19 19:45:03 +0530

    Passport View installation.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,14 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script src="{{ asset('js/app.js') }}"></script>
 @endsection
